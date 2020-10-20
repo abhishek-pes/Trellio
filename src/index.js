0e12107641a21bfd8f274b63622cc881aacf3310
Abhishek4848 <54174670+Abhishek4848@users.noreply.github.com>
2020-10-20 20:40:28 +0530

    Added Navbar

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import SideBar from './components/NavBar'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <SideBar />
   </React.StrictMode>,
   document.getElementById('root')
 );
