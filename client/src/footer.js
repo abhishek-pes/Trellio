d36e323472fb7caab45d69eea3f71e98c718208a
abhishek-pes <abhishekmis402gmail.com>
2020-12-07 16:44:41 +0530

    added wiki

--- client/src/footer.js
+++ client/src/footer.js
@@ -9,7 +9,7 @@ const Footer = () => (
     <p>&copy;{new Date().getFullYear()} Copyright: <a href="https://github.com/Abhishek4848/Trellio"> Team Trellio </a> <br/>
     <a href="https://github.com/Abhishek4848/Trellio" target="_blank"><GitHubIcon/></a>
     <a href="mailto:v.abhishek1347@gmail.com" target="_blank"><MailOutlineIcon/></a>
-    <a href="https://trellio.herokuapp.com/" target="_blank"><LanguageIcon/></a>
+    <a href="https://github.com/Abhishek4848/Trellio/wiki" target="_blank"><LanguageIcon/></a>
     </p>
     </center>
   </div>
